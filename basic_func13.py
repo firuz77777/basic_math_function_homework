ff7be53dc0842c2561143a0bcb30862bdc5d20a4
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-10-21 17:24:35 +0500

    1

--- basic_func13.py
+++ basic_func13.py
@@ -1,6 +1,7 @@
 # Create a function called main.
 # Create function argument a.
 # Returns the square root of a number.
-def main(a = 9):
-    return a/3
-print(main())
+def main(a):
+    x = a / 3
+    return x
+print(main(9))
